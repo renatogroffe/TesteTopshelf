aeb8ae546eb1a55e19710d2afee8d1251a71a7ad
Renato Groffe <renato.groffe@yahoo.com.br>
2016-12-28 01:19:23 -0200

    Add project files.

---
+++ TesteTopshelf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TesteTopshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(p =>
+            {
+                p.Service<ServicoTeste>(s =>
+                {
+                    s.ConstructUsing(st => new ServicoTeste());
+                    s.WhenStarted(st => st.Start());
+                    s.WhenStopped(st => st.Stop());
+                });
+                p.RunAsLocalService();
+
+                p.SetDescription("Exemplo de serviço criado com o Topshelf");
+                p.SetDisplayName("Teste Topshelf");
+                p.SetServiceName("TesteTopshelf");
+            });
+        }
+    }
+}
---
+++ TesteTopshelf/ServicoTeste.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Timers;
+using System.IO;
+
+namespace TesteTopshelf
+{
+    public class ServicoTeste
+    {
+        private Timer _timer;
+
+        public ServicoTeste()
+        {
+            _timer = new Timer(60000);
+            _timer.Elapsed += timer_Elapsed;
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string mensagem =
+                $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} Teste";
+            Console.WriteLine(mensagem);
+
+            using (StreamWriter arquivo =
+                new StreamWriter(@"C:\Temp\Topshelf\Teste.txt", true))
+            {
+                arquivo.WriteLine(mensagem);
+                arquivo.Close();
+            }
+        }
+    }
+}
